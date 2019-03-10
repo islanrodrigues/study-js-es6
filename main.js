80c262dd12d8248e348cd774f81418e61d11fdc6
Islan Rodrigues <jose-islan52@hotmail.com>
2019-03-10 15:49:55 -0300

    Configurando webpack no projeto

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-const user = {
-    nome: "Islan",
-    idade: 21,
-    empresa: "Estudante",
-    endereco: {
-        cidade: "Guarulhos",
-        estado: "SP"
-    },
-};
+import {soma} from './funcoes'
 
-const {nome, idade, endereco:{cidade, estado}} = user;
-
-console.log('Olá! Meu nome é ' + nome + ' e tenho ' + idade + ' anos.');
-
-//Usando Template Literals
-console.log(`Olá! Meu nome é ${nome} e tenho ${idade} anos.`);
-console.log(`Olá! Me chamo ${nome}, tenho ${idade} anos e resido atualmente na cidade de ${cidade}-${estado}`);
+console.log(soma(2,3));
