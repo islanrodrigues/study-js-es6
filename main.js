b92c2a31ff1de6e8959b97c4e443ce70bf1c4bcb
Islan Rodrigues <jose-islan52@hotmail.com>
2019-03-10 18:34:42 -0300

    Organizando os arquivos por pastas e fazendo uso do 'webpack dev server' no projeto

--- main.js
+++
@@ -1,7 +0,0 @@
-import * as functions from './funcoes'
-//import soma, {sub, mult} from './soma'
-
-console.log(functions);
-console.log(functions.soma(1,4));
-console.log(functions.sub(5,2));
-console.log(functions.mult(2,2));
---
+++ src/main.js
@@ -0,0 +1,1 @@
+alert('Acessou! Testando o Webpack Dev Server');
