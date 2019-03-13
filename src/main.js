285753903db49e176534e3ad808f2b1e263f71d6
Islan Rodrigues <jose-islan52@hotmail.com>
2019-03-13 18:40:48 -0300

    Trabalhando com async/await em javascript

--- src/main.js
+++ src/main.js
@@ -1,1 +1,40 @@
-alert('Acessou! Testando o Webpack Dev Server');
+
+const minhaPromise = 
+() => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('OK')
+    }, 2000);
+});
+
+
+// minhaPromise().then(response => {
+//     console.log(response);
+// })
+// .catch(error => {
+
+// });
+
+async function executaPromise1() {
+    const response = await minhaPromise();
+
+    console.log(response);    
+};
+
+
+async function executaPromise2 () {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+};
+
+
+const executaPromise3 = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+
+}
+
+//executaPromise1();
+//executaPromise2();
+executaPromise3();
