f98d42d8a0f868dec49f7e57fcee7b051d0d4c26
Islan Rodrigues <jose-islan52@hotmail.com>
2019-03-16 20:10:45 -0300

    Botão 'adicionar' - aplicando a exibição dos novos elementos da lista em tela (valores ainda estáticos)

--- src/main.js
+++ src/main.js
@@ -3,12 +3,46 @@ class App {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
         this.registerHandlers();
-    }
+    } // closing construtor
+
+
 
     registerHandlers() {
         this.formEl.onsubmit = event => this.addRepository(event);
-    }
+    } //closing method
+
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descripEl = document.createElement('p');
+            descripEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+            linkEl.setAttribute('href', repo.html_url);
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descripEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
+    } //closing method
+
+
 
     addRepository(event) {
         event.preventDefault();
@@ -20,8 +54,10 @@ class App {
             html_url: 'https://github.com/islanrodrigues',
         });
 
-        console.log(this.repositories);
-    }
-}
+        this.render();
+    }// closing method
+
+}//closing class
+
 
 new App();
