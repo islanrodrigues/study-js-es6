f6ade826e724133577778b821fa7674e2f28b4ff
Islan Rodrigues <jose-islan52@hotmail.com>
2019-03-16 22:16:30 -0300

    Renderização de elementos na tela de forma dinâmica - buscando informações de usuários da API do GitHub com axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import api from './api';
+
+
 class App {
     constructor() {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repo]');
         this.listEl = document.getElementById('repo-list');
         this.registerHandlers();
     } // closing construtor
@@ -16,7 +20,7 @@ class App {
 
     render() {
         this.listEl.innerHTML = '';
-
+        
         this.repositories.forEach(repo => {
             let imgEl = document.createElement('img');
             imgEl.setAttribute('src', repo.avatar_url);
@@ -25,12 +29,16 @@ class App {
             titleEl.appendChild(document.createTextNode(repo.name));
 
             let descripEl = document.createElement('p');
-            descripEl.appendChild(document.createTextNode(repo.description));
+            descripEl.appendChild(document.createTextNode(repo.bio));
 
+            let iconEl = document.createElement('i');
+            iconEl.setAttribute('class', 'fab fa-github');
+            
             let linkEl = document.createElement('a');
             linkEl.setAttribute('target', '_blank');
-            linkEl.appendChild(document.createTextNode('Acessar'));
+            linkEl.appendChild(document.createTextNode('Acessar '));
             linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(iconEl);
 
             let listItemEl = document.createElement('li');
             listItemEl.appendChild(imgEl);
@@ -44,15 +52,27 @@ class App {
 
 
 
-    addRepository(event) {
+    async addRepository(event) {
         event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0)
+            return;  
         
+        const response = await api.get(`/users/${repoInput}`);
+        //console.log(response);
+
+        const {name, bio, avatar_url, html_url} = response.data;
+
         this.repositories.push({
-            name: 'Islan Rodrigues',
-            description: '< "A dev in construction 💻☕️ " /> Computer Science Student',
-            avatar_url: 'https://avatars1.githubusercontent.com/u/8366406?s=460&v=4',
-            html_url: 'https://github.com/islanrodrigues',
+            name,
+            bio,
+            avatar_url,
+            html_url,
         });
+
+        this.inputEl.value = '';
 
         this.render();
     }// closing method
